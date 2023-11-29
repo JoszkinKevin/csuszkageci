e7bffa587e9de599d23eee955fdc02b5d6876f1b
JoszkinKevin <54977279+JoszkinKevin@users.noreply.github.com>
2023-11-29 15:37:01 +0100

    Update anyad.py

--- anyad.py
+++ anyad.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
 st.title("csuszka")
-szint = st.slider("valasszal szamot csoves", 0, 16)
+szint = st.slider("valasszal szamot csoves", 0**, 16**)
+
 
 st.text('valasztott: {}'.format(szint))
